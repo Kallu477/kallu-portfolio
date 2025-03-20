7cf7df89eaece0a76cbcb58144c277892dadb436
Kallu477 <meatbst7@gmail.com>
2025-03-20 16:55:49 -0400

    portfolio created

---
+++ src/content_option.js
@@ -0,0 +1,100 @@
+const logotext = "KALYANI KONGALA";
+const meta = {
+    title: "KALYANI KONGALA",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Kalayani Kongala",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+    },
+    description: "Inspired to create web applications that are user-friendly and visually appealing. I am a full-stack developer with experience in both front-end and back-end development. I am passionate about learning new technologies and applying them to real-world problems.",
+    // your_img_url: `profile-pic.png`,
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kalyanierik@domain.com",
+    YOUR_FONE: "(804) 484-2230",
+    description: "Inflicted with a passion for web development and a desire to help others, I have been freelancing for over 2 years. I am a full-stack developer and have experience in many programming languages and web development tools.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Kallu477",
+    gmail: "mailto:kalyanierik@domain.com",
+    linkedin: "https://linkedin.com/in/kalyanierik", //temp for linkedin
+};
+export {
+    meta,
+    dataabout,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
